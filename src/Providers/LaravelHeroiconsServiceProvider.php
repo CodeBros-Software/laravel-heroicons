363a30cb7b191fd48f93f9d62c213b221a3f798b
Norman Reinhard <info@codebros.nl>
2024-01-12 21:16:12 +0100

    refactor service provider

--- src/Providers/LaravelHeroiconsServiceProvider.php
+++ src/Providers/LaravelHeroiconsServiceProvider.php
@@ -6,15 +6,10 @@ use Illuminate\Support\ServiceProvider;
 
 class LaravelHeroiconsServiceProvider extends ServiceProvider
 {
-    public function register()
-    {
-//        for file in *.svg.blade.php
-//do
-//  sed -i 's/width="24" height="24"/{{ $attributes->merge(['class' => 'h-6 w-6']) }}/g' "$file"
-//done
-    }
-
-    public function boot()
+    /**
+     * Register services.
+     */
+    public function boot(): void
     {
         $this->loadViewsFrom(__DIR__.'/../../resources/views', 'heroicons');
     }
